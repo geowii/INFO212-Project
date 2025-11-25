0cd1836f83b400fee82ccfccefb89ae7c406e0a6
Peoli6071 <peoli6071@uib.no>
2025-11-25 08:12:46 +0100

    Update login.js

--- framework/main/control/access/login.js
+++ framework/main/control/access/login.js
@@ -3,10 +3,8 @@
  
  
  
- this.#usernameInput = document.querySelector('.username');
-        this.#passwordInput = document.querySelectorAll('input')[1];
-        this.#registerButton = document.querySelector('#registerButton button');
-        this.#loginButton = document.querySelector('#loginButton button');
+this.#usernameInput
+this.#passwordInput
 
 class LoginController {
     
