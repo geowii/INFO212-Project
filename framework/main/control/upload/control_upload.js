f821078d0e392a1df42a62ed0540566e752360e4
Tarald <taraldwinje@gmail.com>
2025-11-25 10:54:03 +0100

    stuff

---
+++ framework/main/control/upload/control_upload.js
@@ -0,0 +1,12 @@
+dom = {
+    dropArea: document.getElementById('drop_area'),
+    fileInput: document.getElementById('input_file'),
+    uploadBtn: document.getElementById('upload_button'),
+    clearBtn: document.getElementById('clear_button'),
+    fileMeta: document.getElementById('fileMeta'),
+    statusEl: document.getElementById('statusEl'),
+    visualGuide: document.getElementById('visual_guide'),
+    videoPreview: document.getElementById('video_preview')
+};
+
+init();
